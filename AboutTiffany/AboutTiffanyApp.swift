b1c5495f4944bd202857b3a6e71f0a2a77e9ec4f
Tiffany Liu <mazingmonkey2004@gmail.com>
2023-08-21 14:55:57 -0400

    Initial Commit

---
+++ AboutTiffany/AboutTiffanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutTiffanyApp.swift
+//  AboutTiffany
+//
+//  Created by Scholar on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutTiffanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
